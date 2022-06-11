74a5d91dbe1b8afc266c99525a76d18b6ef93eca
Philipe <philipe-leandro@hotmail.com>
2022-06-11 11:14:28 -0300

    feat: Adiciona botão de cadastro para categoria de cliente
    Co-authored-by: Davide Almeida <davide-almeida@users.noreply.github.com>

--- spec/system/client_category/admin_views_client_category_spec.rb
+++ spec/system/client_category/admin_views_client_category_spec.rb
@@ -28,4 +28,25 @@ describe 'Admin views client category' do
     expect(page).to have_content 'Categoria de Clientes'
     expect(page).to have_content 'Não há categoria cadastrada'
   end
+
+  it 'access new_client_category page' do
+    # admin = Admin.create(email: 'example@userubis.com.br', password: 'password', password_confirmation: 'password', full_name: 'Pedrinho Junior Gomes', cpf: '12345678944')
+
+    visit client_categories_path
+    click_on 'Cadastrar categoria'
+    
+    expect(current_path).to eq new_client_category_path
+    expect(page).to have_field 'Nome'
+    expect(page).to have_field 'Porcentagem de desconto'
+  end
+
+  it 'return to client_categories_path' do
+    # admin = Admin.create(email: 'example@userubis.com.br', password: 'password', password_confirmation: 'password', full_name: 'Pedrinho Junior Gomes', cpf: '12345678944')
+
+    visit new_client_category_path
+    click_on 'Voltar'
+    
+    expect(page).to have_content 'Não há categoria cadastrada'
+    expect(page).to have_content 'Categoria de Clientes'
+  end
 end
