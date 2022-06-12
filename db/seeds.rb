417010ae3828e740c80d6203e5dbee12685f647e
Lucas Borges <diasb.trabalho@gmail.com>
2022-06-12 17:19:33 -0300

    feat:  Popular seeds
    Co-authored-by: Jhonny Toledo <jhonnytoledo4975@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts
+puts '--------------------------------------------------'
+puts 'Start "seeds"'
+puts '--------------------------------------------------'
+puts
+
+puts '-------------- creating Admins -------------'
+
+Admin.create!(full_name: 'José Arantes', cpf: '24365465686',
+              email: 'jose@userubis.com.br', password: '123464',
+              status: 0)
+
+active = Admin.create!(full_name: 'Lucio Santos', cpf: '06001818398',
+              email: 'lucio22@userubis.com.br', password: '239102',
+              status: 5)
+
+Admin.create!(full_name: 'Felipe Ferreira', cpf: '64262244563',
+              email: 'feferreira556@userubis.com.br', password: '203942',
+              status: 5)
+
+puts '-------------- creating AdminPermissions -------------'
+
+AdminPermission.create!(admin_id: 1, active_admin: active.id)
+
+puts
+puts '--------------------------------------------------'
+puts 'Finished "seeds"'
+puts '--------------------------------------------------'
+puts
