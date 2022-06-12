8973d02bad856c1ffaeb8dee1cdd8567a281995e
Lucas Borges <diasb.trabalho@gmail.com>
2022-06-12 17:15:42 -0300

    feat: Adicionar migrations
    Migração para adicionar status para a tabela de admin
    Migração para cria a tabela de Permissão de Admin
    Migração para adicionar a coluna active_admin à tabela AdminPermission
    Co-authored-by: Jhonny Toledo <jhonnytoledo4975@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_08_235329) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_12_162631) do
+  create_table "admin_permissions", force: :cascade do |t|
+    t.integer "admin_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "active_admin"
+    t.index ["admin_id"], name: "index_admin_permissions_on_admin_id"
+  end
+
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -21,8 +29,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_08_235329) do
     t.string "cpf", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "admin_permissions", "admins"
 end
