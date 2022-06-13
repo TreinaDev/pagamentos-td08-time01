9ef45269872ad10b775f84e91d37fbca7e1d4c6e
Philipe <philipe-leandro@hotmail.com>
2022-06-13 17:44:58 -0300

    refactor: Refatora teste
    Co-authored-by: Davide Almeida <davide-almeida@users.noreply.github.com>

--- spec/models/client_company_spec.rb
+++ spec/models/client_company_spec.rb
@@ -5,6 +5,12 @@ require 'rails_helper'
 RSpec.describe ClientCompany, type: :model do
   describe '#valid?' do
     context 'when presence' do
+      it 'with success' do
+        client_company = described_class.new(company_name: 'ACME LTDA', cnpj: '12345678998745')
+
+        expect(client_company.valid?).to be true
+      end
+
       it 'false when company_name is empty' do
         client_company = described_class.new(company_name: '', cnpj: '12345678998745')
 
@@ -15,12 +21,6 @@ RSpec.describe ClientCompany, type: :model do
         client_company = described_class.new(company_name: 'ACME LTDA', cnpj: '')
 
         expect(client_company.valid?).to be false
-      end
-
-      it 'with success' do
-        client_company = described_class.new(company_name: 'ACME LTDA', cnpj: '12345678998745')
-
-        expect(client_company.valid?).to be true
       end
     end
   end
