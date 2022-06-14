6170ed419242861754d25d5c3e7ec7b36aff5981
Davide Almeida <contato@davidealmeida.com.br>
2022-06-14 10:23:38 -0300

    feat: Adiciona autenticação em client_category
    Co-authored-by: Philipe Leandro <philipe-leandro@hotmail.com>

--- spec/system/client_category/admin_registers_client_category_spec.rb
+++ spec/system/client_category/admin_registers_client_category_spec.rb
@@ -4,16 +4,24 @@ require 'rails_helper'
 
 describe 'admin registers client category' do
   it 'with success' do
+    admin = Admin.create(email: 'example@userubis.com.br', password: 'password', password_confirmation: 'password',
+                         full_name: 'Pedrinho Junior Gomes', cpf: '12345678944')
+
+    login_as(admin)
     visit new_client_category_path
 
     fill_in 'Nome',	with: 'Bronze'
-    fill_in 'Porcentagem de desconto',	with: 'Ouro'
+    fill_in 'Porcentagem de desconto', with: 'Ouro'
     click_on 'Cadastrar'
 
     expect(page).to have_content 'Categoria criada com sucesso.'
   end
 
   it 'with fail' do
+    admin = Admin.create(email: 'example@userubis.com.br', password: 'password', password_confirmation: 'password',
+                         full_name: 'Pedrinho Junior Gomes', cpf: '12345678944')
+
+    login_as(admin)
     visit new_client_category_path
 
     fill_in 'Nome',	with: 'Diamante'
--- spec/system/client_category/admin_views_client_category_spec.rb
+++ spec/system/client_category/admin_views_client_category_spec.rb
@@ -4,9 +4,12 @@ require 'rails_helper'
 
 describe 'Admin views client category' do
   it 'with success' do
+    admin = Admin.create(email: 'example@userubis.com.br', password: 'password', password_confirmation: 'password',
+                         full_name: 'Pedrinho Junior Gomes', cpf: '12345678944')
     ClientCategory.create!(name: 'Bronze', discount_percent: 0)
     ClientCategory.create!(name: 'Ouro', discount_percent: 10)
 
+    login_as(admin)
     visit client_categories_path
 
     expect(page).to have_content 'Bronze'
@@ -16,6 +19,10 @@ describe 'Admin views client category' do
   end
 
   it 'without any client categories' do
+    admin = Admin.create(email: 'example@userubis.com.br', password: 'password', password_confirmation: 'password',
+                         full_name: 'Pedrinho Junior Gomes', cpf: '12345678944')
+
+    login_as(admin)
     visit client_categories_path
 
     expect(page).to have_content 'Categoria de Clientes'
@@ -23,19 +30,32 @@ describe 'Admin views client category' do
   end
 
   it 'access new_client_category page' do
+    admin = Admin.create(email: 'example@userubis.com.br', password: 'password', password_confirmation: 'password',
+                         full_name: 'Pedrinho Junior Gomes', cpf: '12345678944')
+
+    login_as(admin)
     visit client_categories_path
     click_on 'Cadastrar categoria'
 
-    expect(current_path).to eq new_client_category_path
+    expect(page).to have_current_path new_client_category_path, ignore_query: true
     expect(page).to have_field 'Nome'
     expect(page).to have_field 'Porcentagem de desconto'
   end
 
   it 'return to client_categories_path' do
-    visit new_client_category_path
-    click_on 'Voltar'
+    admin = Admin.create(email: 'example@userubis.com.br', password: 'password', password_confirmation: 'password',
+                         full_name: 'Pedrinho Junior Gomes', cpf: '12345678944')
+
+    login_as(admin)
+    visit client_categories_path
 
     expect(page).to have_content 'Não há categoria cadastrada'
     expect(page).to have_content 'Categoria de Clientes'
+  end
+
+  it 'access client_category without be authenticated' do
+    visit client_categories_path
+
+    expect(page).to have_content 'Para continuar, faça login ou registre-se.'
   end
 end
