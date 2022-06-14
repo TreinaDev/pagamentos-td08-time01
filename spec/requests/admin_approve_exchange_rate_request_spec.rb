9d67442adf4a9e30fe35287220516e11cda1582e
Júnior <b.sep@live.com>
2022-06-14 10:48:24 -0300

    test: refatoração de testes

--- spec/requests/admin_approve_exchange_rate_request_spec.rb
+++ spec/requests/admin_approve_exchange_rate_request_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe 'admin recuse exchange rate' do
+describe 'admin approve exchange rate' do
   it 'successfully' do
     admin = create(:admin)
     admin2 = create(:admin, email: 'b2@userubis.com.br', cpf: '57079167094')
@@ -10,20 +10,22 @@ describe 'admin recuse exchange rate' do
     er = create(:exchange_rate, brl_coin: 6, created_by: admin, register_date: 3.days.from_now)
 
     login_as admin2
-    post(recused_exchange_rate_path(er),
-         params: { 'controller' => 'exchange_rates', 'action' => 'recused', 'id' => '2' })
+    post(approved_exchange_rate_path(er),
+         params: { 'controller' => 'exchange_rates', 'action' => 'approved', 'id' => '2' })
 
     expect(response).to redirect_to exchange_rate_path(er)
     expect(assert_response(:redirect)).to be true
-    expect(ExchangeRate.last.status).to eq 'recused'
+    expect(ExchangeRate.last.status).to eq 'approved'
   end
 
   it 'and isnt logged in' do
     admin = create(:admin)
-    er = create(:exchange_rate, created_by: admin)
+    create(:exchange_rate, brl_coin: 5, created_by: admin, register_date: 2.days.from_now)
+    er = create(:exchange_rate, brl_coin: 6, created_by: admin, register_date: 3.days.from_now, status: 'pending')
 
-    post(recused_exchange_rate_path(er))
+    post(approved_exchange_rate_path(er))
 
     expect(response).to redirect_to new_admin_session_path
+    expect(ExchangeRate.last.status).to eq 'pending'
   end
 end
