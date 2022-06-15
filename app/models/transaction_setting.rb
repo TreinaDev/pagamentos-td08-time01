0d7530d1cfcd348e60171a1ae24765dbac16fe51
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-06-15 10:37:29 -0300

    feat: Adicionar validações
    Co-authored-by: Davide Almeida <davide-almeida@users.noreply.github.com>

--- app/models/transaction_setting.rb
+++ app/models/transaction_setting.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class TransactionSetting < ApplicationRecord
+  validates :max_credit, presence: true
+  validates :max_credit, numericality: { greater_than_or_equal_to: 0 }
 end
