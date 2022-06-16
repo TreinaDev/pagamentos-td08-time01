f0e36ce051db3d2f09245c38d344e6ade5978651
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-06-16 12:18:51 -0300

    fix: Corrigir seeds
    Co-authored-by: davide-almeida <davide.jhonatan@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,8 @@
-# frozen_string_literal: true
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 puts '--------------------------------------------------'
 puts 'Start "seeds"'
 puts '--------------------------------------------------'
 
-puts '----------------- creating Admins ----------------'
+puts '------------ creating Admins ---------------------'
 Admin.create!(full_name: 'José Arantes', cpf: '24365465686',
               email: 'jose@userubis.com.br', password: '123464',
               status: 0)
@@ -26,15 +16,19 @@ Admin.create!(full_name: 'Felipe Ferreira', cpf: '64262244563',
 puts '------------ creating AdminPermissions -----------'
 AdminPermission.create!(admin_id: 1, active_admin: active.id)
 
-puts '------------ creating ClientCategory ------------'
+puts '------------ creating ClientCategory -------------'
 ClientCategory.create!(name: "Bronze", discount_percent: 0)
 ClientCategory.create!(name: "Ouro", discount_percent: 10)
 
-puts '------------ creating ClientPerson -------------'
+puts '------------ creating Client ---------------------'
 Client.create!(client_type: 0, client_category_id: 1)
+Client.create!(client_type: 5, client_category_id: 1)
 
-puts '------------ creating ClientPerson -------------'
-ClientPerson.create!(full_name: 'Zeca Urubú', cpf: '12345678999')
+puts '------------ creating ClientPerson ---------------'
+ClientPerson.create!(full_name: 'Zeca Urubú', cpf: '12345678999', client_id: 1)
+
+puts '------------ creating ClientCompany --------------'
+ClientCompany.create!(company_name: 'ACME LTDA', cnpj: '12345678912345', client_id: 2)
 
 puts '--------------------------------------------------'
 puts 'Finished "seeds"'
