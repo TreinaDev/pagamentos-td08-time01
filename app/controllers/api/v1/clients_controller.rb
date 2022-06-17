5892267591365d0eb5aae8e39268fcffef259d5d
Lucas Borges <diasb.trabalho@gmail.com>
2022-06-17 19:24:10 -0300

    feat: Cria disponibilização de dados
    Co-authored-by: Philipe Leandro <philipe-leandro@hotmail.com>
    Co-authored-by: Júnior Beto <b-sep@users.noreply.github.com>

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,15 +1,37 @@
-# frozen_string_literal: true
+    # frozen_string_literal: true
 
 module Api
   module V1
     class ClientsController < ActionController::API
       def show
-        client = Client.find(params[:id])
+        registration_number = params[:registration_number]
+        if CPF.valid?(registration_number, strict: true)
+          render_client_person(registration_number)
+        elsif CNPJ.valid?(registration_number, strict: true)
+          render_client_company(registration_number)
+        else
+          render json: { errors: 'Cliente não encontrado' }, status: :not_found
+        end
+      end
+
+      private
+
+      def render_client_person(cpf)
+        client_person = ClientPerson.find_by(cpf: cpf)
+        client = client_person.client
+        client_category = client.client_category
         render json: [client.as_json(except: %i[created_at updated_at client_category_id id client_type]),
-                      client.client_category.as_json(except: %i[created_at updated_at id]),
-                      client.client_people.as_json(except: %i[created_at updated_at client_id id])]
-      rescue StandardError
-        render json: { errors: 'Cliente não encontrado' }, status: :not_found
+                      client_category.as_json(except: %i[created_at updated_at id]),
+                      client_person.as_json(except: %i[created_at updated_at client_id id])]
+      end
+
+      def render_client_company(cnpj)
+        client_company = ClientCompany.find_by(cnpj: cnpj)
+        client = client_company.client
+        client_category = client.client_category
+        render json: [client.as_json(except: %i[created_at updated_at client_category_id id client_type]),
+                      client_category.as_json(except: %i[created_at updated_at id]),
+                      client_company.as_json(except: %i[created_at updated_at client_id id])]
       end
     end
   end
--- spec/requests/api/clients_data/api_expose_a_client_data_spec.rb
+++ spec/requests/api/clients_data/api_expose_a_client_data_spec.rb
@@ -4,12 +4,12 @@ require 'rails_helper'
 
 describe 'Pagamento API' do
   context 'when GET /api/v1/clients/1' do
-    it 'success' do
+    it 'success as client person' do
       client_category = ClientCategory.create!(name: 'Bronze', discount_percent: 0)
       client = Client.create!(client_type: 0, client_category_id: client_category.id, balance: 0)
-      ClientPerson.create!(full_name: 'Jossoandenson Kirton', cpf: '277.759.424-44', client_id: client.id)
+      client_person = ClientPerson.create!(full_name: 'Jossoandenson Kirton', cpf: '277.759.424-44', client_id: client.id)
 
-      get "/api/v1/clients/#{client.id}"
+      get "/api/v1/clients/1", params: {registration_number: client_person.cpf}
       json_response = JSON.parse(response.body)
 
       expect(response.status).to eq 200
@@ -17,12 +17,34 @@ describe 'Pagamento API' do
       expect(json_response[0]['balance']).to eq 0.0
       expect(json_response[1]['name']).to eq 'Bronze'
       expect(json_response[1]['discount_percent']).to eq 0
-      expect(json_response[2][0]['full_name']).to eq 'Jossoandenson Kirton'
-      expect(json_response[2][0]['cpf']).to eq '277.759.424-44'
+      expect(json_response[2]['full_name']).to eq 'Jossoandenson Kirton'
+      expect(json_response[2]['cpf']).to eq '277.759.424-44'
       expect(json_response[0].count).to eq 1
       expect(json_response[1].count).to eq 2
-      expect(json_response[2][0].count).to eq 2
+      expect(json_response[2].count).to eq 2
     end
+
+    it 'success as client company' do
+      client_category = ClientCategory.create!(name: 'Bronze', discount_percent: 0)
+      client = Client.create!(client_type: 0, client_category_id: client_category.id, balance: 0)
+      client_company = ClientCompany.create!(company_name: 'ACME LTDA', cnpj: '71721257678217', client_id: client.id)
+
+      get "/api/v1/clients/1", params: {registration_number: client_company.cnpj}
+      json_response = JSON.parse(response.body)
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      expect(json_response[0]['balance']).to eq 0.0
+      expect(json_response[1]['name']).to eq 'Bronze'
+      expect(json_response[1]['discount_percent']).to eq 0
+      expect(json_response[2]['company_name']).to eq 'ACME LTDA'
+      expect(json_response[2]['cnpj']).to eq '71721257678217'
+      #71.721.257/6782-17
+      expect(json_response[0].count).to eq 1
+      expect(json_response[1].count).to eq 2
+      expect(json_response[2].count).to eq 2
+    end
+
 
     it 'fail' do
       get '/api/v1/clients/2'
