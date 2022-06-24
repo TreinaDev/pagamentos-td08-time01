2a8287e3e288bb4f84cd6750113e0cff2e0105c6
Davide Almeida <contato@davidealmeida.com.br>
2022-06-24 12:15:36 -0300

    refactor: Refatora página inicial
    Co-authored-by: Philipe Leandro <philipe-leandro@hotmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,9 @@
 
 class HomeController < ApplicationController
   before_action :authenticate_admin!
-  def index
-    @client_transactions_count = ClientTransaction.where(status: :pending).count
-    @admin_count = Admin.where(status: 0).count
 
-    if ExchangeRate.where(status: 'approved').any?
-      @exchange_rate = ExchangeRate.where(status: 'approved').last.rubi_coin
-    else
-      @exchange_rate = 'Sem cadastro'
-    end
+  def index
+    @client_transactions_count = ClientTransaction.pending.count
+    @admin_count = Admin.pending.count
   end
 end
