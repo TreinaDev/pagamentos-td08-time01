e34e14a6f728e709dafa5a637561fd049ec93320
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-06-27 16:44:59 -0300

    refactor: Refatorações testes e client controller

--- spec/requests/api/v1/clients_data/api_expose_a_client_data_spec.rb
+++ spec/requests/api/v1/clients_data/api_expose_a_client_data_spec.rb
@@ -2,17 +2,17 @@
 
 require 'rails_helper'
 
-describe 'Pagamento API' do
-  context 'when GET /api/v1/clients_info' do
-    it 'success as client person' do
+describe 'GET /api/v1/clients_info' do
+  context 'when params is valid' do
+    it 'with client person' do
       client_category = ClientCategory.create!(name: 'Bronze', discount_percent: 0)
       client = Client.create!(client_type: 0, client_category_id: client_category.id, balance: 0)
       client_person = ClientPerson.create!(full_name: 'Jossoandenson Kirton', cpf: '277.759.424-44',
                                            client_id: client.id)
 
       get '/api/v1/clients_info', params: { registration_number: client_person.cpf }
-      json_response = JSON.parse(response.body)
 
+      json_response = JSON.parse(response.body)
       expect(response.status).to eq 200
       expect(response.content_type).to include 'application/json'
       expect(json_response['client_balance']['balance']).to eq 0.0
@@ -22,14 +22,14 @@ describe 'Pagamento API' do
       expect(json_response['client_info'].count).to eq 2
     end
 
-    it 'success as client company' do
+    it 'with client company' do
       client_category = ClientCategory.create!(name: 'Bronze', discount_percent: 0)
       client = Client.create!(client_type: 0, client_category_id: client_category.id, balance: 0)
       client_company = ClientCompany.create!(company_name: 'ACME LTDA', cnpj: '07638546899424', client_id: client.id)
 
       get '/api/v1/clients_info', params: { registration_number: client_company.cnpj }
-      json_response = JSON.parse(response.body)
 
+      json_response = JSON.parse(response.body)
       expect(response.status).to eq 200
       expect(response.content_type).to include 'application/json'
       expect(json_response['client_balance']['balance']).to eq 0.0
@@ -38,13 +38,23 @@ describe 'Pagamento API' do
       expect(json_response['client_balance'].count).to eq 1
       expect(json_response['client_info'].count).to eq 2
     end
+  end
 
-    it 'fail' do
+  context 'with fail' do
+    it 'when params is empty' do
       get '/api/v1/clients_info'
-      json_response = JSON.parse(response.body)
 
-      expect(response.status).to eq 404
-      expect(json_response['errors']).to include 'Cliente não encontrado'
+      json_response = JSON.parse(response.body)
+      expect(response.status).to eq 422
+      expect(json_response['message']).to include 'O numero de indentificação é inválido'
+    end
+
+    it 'when params is invalid' do
+      get '/api/v1/clients_info', params: { registration_number: 'awdawdawd' }
+
+      json_response = JSON.parse(response.body)
+      expect(response.status).to eq 422
+      expect(json_response['message']).to include 'O numero de indentificação é inválido'
     end
   end
 end
