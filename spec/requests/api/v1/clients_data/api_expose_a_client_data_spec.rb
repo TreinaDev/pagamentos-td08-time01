ea66a003dbc047ff238a5d468ddc0af0d79d5674
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-06-28 17:36:14 -0300

    refactor: Refatorar expectativas
    Co-authored-by: Lucas Borges <LucasDLAB@users.noreply.github.com>
    Co-authored-by: Júnior Beto <b-sep@users.noreply.github.com>

--- spec/requests/api/v1/clients_data/api_expose_a_client_data_spec.rb
+++ spec/requests/api/v1/clients_data/api_expose_a_client_data_spec.rb
@@ -12,17 +12,20 @@ describe 'GET /api/v1/clients_info' do
         client.client_bonus_balances.create!(bonus_value: 100, expire_date: Time.zone.today)
       end
 
-      client.client_bonus_balances.create!(bonus_value: 100, expire_date: 2.weeks.from_now)
-      client.client_bonus_balances.create!(bonus_value: 200, expire_date: 3.weeks.from_now)
+      bonus_one = client.client_bonus_balances.create!(bonus_value: 100, expire_date: 2.weeks.from_now)
+      bonus_two = client.client_bonus_balances.create!(bonus_value: 200, expire_date: 3.weeks.from_now)
       transaction = create(:client_transaction, client: client, status: 'approved', approval_date: Time.zone.now)
       client_person = ClientPerson.create!(full_name: 'Jossoandenson Kirton', cpf: '277.759.424-44',
                                            client_id: client.id)
+      bonus_one_response = { 'bonus_value' => 100.0, 'expire_date' => bonus_one.expire_date.strftime('%Y-%m-%d') }
+      bonus_two_response = { 'bonus_value' => 200.0, 'expire_date' => bonus_two.expire_date.strftime('%Y-%m-%d') }
+
       client_transaction = {
         credit_value: transaction.credit_value,
         type_transaction: transaction.type_transaction,
-        transaction_date: transaction.transaction_date,
+        transaction_date: transaction.transaction_date.strftime('%d/%m/%Y ás %H:%M'),
         status: transaction.status,
-        approval_date: transaction.approval_date,
+        approval_date: transaction.approval_date.strftime('%d/%m/%Y ás %H:%M'),
         code: transaction.code
       }.as_json
 
@@ -31,11 +34,12 @@ describe 'GET /api/v1/clients_info' do
       json_response = JSON.parse(response.body)
       expect(response.status).to eq 200
       expect(response.content_type).to include 'application/json'
-      expect(json_response['client_balance']['balance']).to eq 500
-      expect(json_response['client_bonus'].count).to eq 2
-      expect(json_response['client_info']['full_name']).to eq 'Jossoandenson Kirton'
-      expect(json_response['client_info'].count).to eq 1
-      expect(json_response['client_transactions']).to include client_transaction
+      expect(json_response['client_info']['balance']).to eq 500
+      expect(json_response['client_info']['bonus'].first).to eq(bonus_one_response)
+      expect(json_response['client_info']['bonus'].last).to eq(bonus_two_response)
+      expect(json_response['client_info']['name']).to eq 'Jossoandenson Kirton'
+      expect(json_response['client_info']['transactions'].count).to eq 1
+      expect(json_response['client_info']['transactions'].last).to include client_transaction
     end
 
     it 'with client company' do
@@ -53,12 +57,10 @@ describe 'GET /api/v1/clients_info' do
       json_response = JSON.parse(response.body)
       expect(response.status).to eq 200
       expect(response.content_type).to include 'application/json'
-      expect(json_response['client_balance']['balance']).to eq 0.0
-      expect(json_response['client_info']['company_name']).to eq 'ACME LTDA'
-      expect(json_response['client_bonus']).to be_empty
-      expect(json_response['client_transactions']).to be_empty
-      expect(json_response['client_balance'].count).to eq 1
-      expect(json_response['client_info'].count).to eq 1
+      expect(json_response['client_info']['balance']).to eq 0.0
+      expect(json_response['client_info']['name']).to eq 'ACME LTDA'
+      expect(json_response['client_info']['bonus']).to be_empty
+      expect(json_response['client_info']['transactions']).to be_empty
     end
   end
 
