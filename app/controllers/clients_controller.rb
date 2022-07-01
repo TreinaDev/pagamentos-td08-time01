7feb9dff4551ec9d94538ac418bb51d717d0e5c1
Davide Almeida <davide.jhonatan@gmail.com>
2022-07-01 18:05:49 -0300

    feat: Adiciona mudança de categoria de cliente
    Co-authored-by: Philipe Leandro <philipe-leandro@hotmail.com>

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,8 +2,31 @@
 
 class ClientsController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_params, only: %i[edit update]
 
   def index
     @clients = Client.all
+  end
+
+  def edit
+    select_client_categories
+  end
+
+  def update
+    return redirect_to clients_path, notice: 'Cliente atualizado com sucesso!' if @client.update(client_params)
+  end
+
+  private
+
+  def set_params
+    @client = Client.find(params[:id])
+  end
+
+  def select_client_categories
+    @client_categories = ClientCategory.all
+  end
+
+  def client_params
+    params.require(:client).permit(:client_category_id)
   end
 end
